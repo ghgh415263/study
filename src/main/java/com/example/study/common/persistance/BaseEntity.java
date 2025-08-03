435ea53a03e10817ed2772c431052d56bb0566bb
Kim JaeYeon <48614095+ghgh415263@users.noreply.github.com>
2025-08-03 12:30:36 +0900

    GH-4 Entity들의 히스토리를 자동저장하는 Spring Data Envers 추가 (#5)
    Entity들의 히스토리를 자동저장하는 Spring Data Envers 추가하고
    Member에 적용하고 저장 테스트 코드 작성

--- src/main/java/com/example/study/common/persistance/BaseEntity.java
+++ src/main/java/com/example/study/common/persistance/BaseEntity.java
@@ -1,15 +1,17 @@
-package com.example.study.common;
+package com.example.study.common.persistance;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Audited
 @Getter
 @MappedSuperclass  // 이 클래스를 상속한 엔티티가 필드를 컬럼으로 인식하게 함
 @EntityListeners(AuditingEntityListener.class) // JPA Auditing 기능을 활성화
