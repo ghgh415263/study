9f53056229ff97d6a60292554b748ab3ff5e8ab5
Kim JaeYeon <48614095+ghgh415263@users.noreply.github.com>
2025-08-16 09:19:22 +0900

    GH-10 회원 탈퇴 기능 추가 (#11)
    * GH-10 회원 탈퇴 기능 추가
    
    - Envers를 이용해 회원 탈퇴 이력을 기록하는 기능 구현 (삭제한 사람을 기록하기 위해 flush추가)
    - 테스트 코드에서 TestFactory 클래스를 활용해 다양한 상태의 회원 객체를 생성하도록 구현
    
    * GH-10 회원 탈퇴 기능 추가
    
    - 회원id를 쓰레드 로컬에서 찾아오게 만듬.

---
+++ src/main/java/com/example/study/member/ui/MemberWithdrawController.java
@@ -0,0 +1,28 @@
+package com.example.study.member.ui;
+
+import com.example.study.common.ApiSuccessResponse;
+import com.example.study.common.LoginMemberContext;
+import com.example.study.member.command.application.MemberWithdrawService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberWithdrawController {
+
+    private final MemberWithdrawService memberWithdrawService;
+
+    private final LoginMemberContext loginMemberContext;
+
+    @DeleteMapping
+    public ApiSuccessResponse<Void> withdraw() {
+        memberWithdrawService.withdrawMember(UUID.fromString(loginMemberContext.getLoginId()));
+        return ApiSuccessResponse.empty();
+    }
+}
